9cfb42ba2c66883844da18429824f2ca5bf831dc
Xiaoyu Lai <lai040630@outlook.com>
2025-09-21 02:13:33 -0400

    v2

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 function App() {
   return (
     <div style={{ fontFamily: "sans-serif", textAlign: "center", marginTop: 80 }}>
-      <h1>Hello World</h1>
+      <h1>Hello World - v2</h1>
       <p>AWS Amplify + GitHub Auto Deploy ✅</p>
     </div>
   );
