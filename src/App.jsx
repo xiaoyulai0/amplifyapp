73d93d6016cc79001fe035e3f7f792bb5a60903d
Xiaoyu Lai <lai040630@outlook.com>
2025-09-21 01:58:53 -0400

    Lab 3: Hello World

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div style={{ fontFamily: "sans-serif", textAlign: "center", marginTop: 80 }}>
+      <h1>Hello World</h1>
+      <p>AWS Amplify + GitHub Auto Deploy ✅</p>
+    </div>
+  );
+}
+export default App;
